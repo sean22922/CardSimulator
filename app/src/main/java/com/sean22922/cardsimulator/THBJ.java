2b067b3eaafe785eb874c21e9ef4d31b4586a5a7
maple3142 <kirby741852963@gmail.com>
2017-06-08 19:48:43 +0800

    新增天華百劍抽獎

--- app/src/main/java/com/sean22922/cardsimulator/THBJ.java
+++ app/src/main/java/com/sean22922/cardsimulator/THBJ.java
@@ -7,11 +7,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class THBJ extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
+    private THBJ.OnFragmentInteractionListener mListener;
+    private EditText times;
+    private Button chou,chou1,chou10;
+    private ListView result;
+    private ProbabilityRandom pr;
+    private ArrayAdapter<String> listAdapter;
 
     public THBJ() {
         // Required empty public constructor
@@ -34,8 +44,47 @@ public class THBJ extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_thbj, container, false);
+        View view=inflater.inflate(R.layout.fragment_thbj, container, false);
+        times=(EditText) view.findViewById(R.id.times);
+        chou=(Button)view.findViewById(R.id.chou);
+        chou1=(Button)view.findViewById(R.id.chou1);
+        chou10=(Button)view.findViewById(R.id.chou10);
+        result=(ListView)view.findViewById(R.id.result);
+        chou.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                int t=1;
+                try{
+                    t=Integer.parseInt(times.getText().toString());
+                }catch (Exception  e){
+                    Toast.makeText(getView().getContext(),getString(R.string.wrongtimes),Toast.LENGTH_SHORT);
+                }
+                listAdapter=new ArrayAdapter<String>(result.getContext(),R.layout.result_list,pr.randList(t));
+                result.setAdapter(listAdapter);
+            }
+        });
+        chou1.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                listAdapter=new ArrayAdapter<String>(result.getContext(),R.layout.result_list,pr.randList(1));
+                result.setAdapter(listAdapter);
+            }
+        });
+        chou10.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                listAdapter=new ArrayAdapter<String>(result.getContext(),R.layout.result_list,pr.randList(10));
+                result.setAdapter(listAdapter);
+            }
+        });
+        pr=new ProbabilityRandom();
+        pr.add(getString(R.string.thbj_weapon_ur),0.03);
+        pr.add(getString(R.string.thbj_weapon_sr),0.12);
+        pr.add(getString(R.string.thbj_armor_ur),0.07);
+        pr.add(getString(R.string.thbj_armor_sr),0.3);
+        pr.add(getString(R.string.thbj_armor_r),0.48);
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
