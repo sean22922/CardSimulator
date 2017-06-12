e2523fd371b8ad3dc6a4ffaa114633f91b20db1c
maple3142 <kirby741852963@gmail.com>
2017-06-12 21:48:01 +0800

    add license dialog

--- app/src/main/java/com/sean22922/cardsimulator/MainActivity.java
+++ app/src/main/java/com/sean22922/cardsimulator/MainActivity.java
@@ -17,6 +17,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
 import android.widget.TextView;
+import de.psdev.licensesdialog.*;
+import de.psdev.licensesdialog.licenses.ApacheSoftwareLicense20;
+import de.psdev.licensesdialog.model.Notice;
+import de.psdev.licensesdialog.model.Notices;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
@@ -89,7 +93,17 @@ public class MainActivity extends AppCompatActivity
                         public void onClick(DialogInterface dialog, int which) {
 
                         }
-                    }).show().findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
+                    })
+                    .show().findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
+            return true;
+        }
+        if(id == R.id.license){
+            Notices ns=new Notices();
+            ns.addNotice(new Notice("MaterialTabHost","https://github.com/yanzm/MaterialTabHost","Copyright 2014 Yuki Anzai",new ApacheSoftwareLicense20()));
+            ns.addNotice(new Notice("LicensesDialog ","https://github.com/PSDev/LicensesDialog","Copyright 2013-2017 Philip Schiffer",new ApacheSoftwareLicense20()));
+            new LicensesDialog.Builder(this)
+                    .setNotices(ns)
+                    .build().show();
             return true;
         }
 
